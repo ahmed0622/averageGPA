4c5b9fd62239b33efbb5a2c8e1bee58b01e31491
Ahmed Abdi <149621098+ahmed0622@users.noreply.github.com>
2025-09-09 00:04:05 -0700

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+       double[] student1Grades = {2.2, 3,4 ,2.1, 4.0 ,2.0};
+       double[] student2Grades = {3.7, 3.4 ,3.1 ,3.0 ,4.0 ,4.0};
+        System.out.println(getGpa(student1Grades));
+        System.out.println(getGpa(student2Grades));
+    }
+    public static double getGpa(double[] arr) {
+        double averageGpa = 0;
+        for (int i = 0; i < arr.length; i++) {
+                System.out.println(arr[i]);
+                averageGpa += arr[i];
+            }
+        return averageGpa / arr.length;
+    }
+    }
